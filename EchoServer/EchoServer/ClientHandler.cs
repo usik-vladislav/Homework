12b1e6d07b2a1085810a01f16d03db65e9d8c3b7
usik-vladislav <31969107+usik-vladislav@users.noreply.github.com>
2019-12-21 02:20:10 +0300

    фпф

---
+++ EchoServer/EchoServer/ClientHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    internal class ClientHandler
+    {
+        private readonly Socket _socket;
+
+        public ClientHandler(Socket socket)
+        {
+            _socket = socket;
+        }
+
+        public void Process()
+        {
+            try
+            {
+                while (true)
+                {
+                    var data = new byte[256];
+
+                    _socket.Receive(data);
+                    _socket.Send(data);
+                }
+            }
+            catch (SocketException ex)
+            {
+                if (ex.ErrorCode == 10054 || ex.ErrorCode == 10053)
+                {
+                    Console.WriteLine("Кто-то отключился...");
+                    _socket.Shutdown(SocketShutdown.Both);
+                    _socket.Close();
+                }
+                else
+                {
+                    Console.WriteLine(ex);
+                    throw;
+                }
+            }
+        }
+    }
+}
